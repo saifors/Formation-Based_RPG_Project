bf13d0ae0e6de06602242ab721112e2b3ccc67ca
Dion Schreuder <40234847+saifors@users.noreply.github.com>
2018-09-28 16:01:44 +0200

    Command CreatCharactersStats
    Command to save CharacterStats for future use in multiple scenes

---
+++ Project SRPGesque/Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void CreateCharacterStats(char charID, int hp, int mp, int atk, int def, int spd)
+	{
+		PlayerPrefs.SetInt(charID + "Hp", hp);
+		PlayerPrefs.SetInt(charID + "Mp", mp);
+		PlayerPrefs.SetInt(charID + "Attack", atk);
+		PlayerPrefs.SetInt(charID + "Defense", def);
+		PlayerPrefs.SetInt(charID + "Speed", spd);
+	}
+}
