97259138ae37764a1ef65ecf60d275eee91147cc
Dion Schreuder <40234847+saifors@users.noreply.github.com>
2018-11-16 13:20:11 +0100

    minor stuff

--- Project SRPGesque/Assets/Scripts/GameManager.cs
+++ Project SRPGesque/Assets/Scripts/GameManager.cs
@@ -6,21 +6,26 @@ public class GameManager : MonoBehaviour
 {
 	public Transform cam_T;
 	public OWPlayerController playerController;
+    public enum CameraSetting { OverworldCam, BattleCam, CutsceneCam}; public CameraSetting camSet;
 	
 	// Use this for initialization
 	void Start () 
 	{
 		cam_T = GameObject.FindGameObjectWithTag("CamTarget").GetComponent<Transform>();
-		playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<OWPlayerController>();
+		playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<OWPlayerController>(); //Ask about whether there's some way for this to workd or whether it's better to split Game manager into OverworldManager and battleManager
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if(playerController.isMoving)
-		{
-			cam_T.position = playerController.trans.position;
-		}
+        if(camSet == CameraSetting.OverworldCam)
+        {
+            if (playerController.isMoving)
+		    {
+			    cam_T.position = playerController.trans.position;
+		    }
+        }
+        
 		
 	}
 }
