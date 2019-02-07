134d807ae598635675eb87e95758011a7b3b87ac
Dion Schreuder <40234847+saifors@users.noreply.github.com>
2019-02-07 13:30:46 +0100

    more setup for dialogue

---
+++ Project SRPGesque/Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueBox : MonoBehaviour
+{
+	public Image[] portraits;
+	public RectTransform textBox;
+	public TextMeshProUGUI textBoxText;
+	public TextMeshProUGUI[] options;
+	public RectTransform[] optionsTrans;
+	// Start is called before the first frame update
+    void Start()
+    {
+		optionsTrans = new RectTransform[options.Length];
+		for(int i = 0; i < optionsTrans.Length; i++)
+		{
+			optionsTrans[i] = options[i].GetComponent<RectTransform>();
+		}
+    }
+
+}
